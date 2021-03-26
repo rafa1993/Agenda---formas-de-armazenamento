b38bc54fb991f33e3a7c17b1a84610184bcea8a1
Rafaela Hristidis Morais <rafaelamorais@brq.com>
2021-03-26 11:09:40 -0300

    migration complexa

--- app/src/main/java/com/brq/agenda/model/Aluno.java
+++ app/src/main/java/com/brq/agenda/model/Aluno.java
@@ -13,7 +13,6 @@ public class Aluno implements Serializable {
     @PrimaryKey(autoGenerate = true)
     private int id = 0;
     private String nome;
-    private String sobrenome;
     private String telefone;
     private String email;
 
@@ -27,10 +26,6 @@ public class Aluno implements Serializable {
     public Aluno() {
 
     }
-
-   public String getSobrenome() {return sobrenome;}
-
-   public void setSobrenome(String sobrenome) {this.sobrenome = sobrenome;}
 
     public void setNome(String nome) {
         this.nome = nome;
@@ -74,8 +69,6 @@ public class Aluno implements Serializable {
         return id > 0;
     }
 
-    public String getNomeCompleto() {
-        return nome + " " + sobrenome;
-    }
+    public String getNomeCompleto() {return nome;}
 }
 
--- app/src/main/java/com/brq/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/com/brq/agenda/ui/activity/FormularioAlunoActivity.java
@@ -19,7 +19,6 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     public static final String TITULO_APPBAR_NOVO_ALUNO = "Novo Aluno";
     private static final String TITULO_APPBAR_EDITA_ALUNO = "Edita Aluno" ;
     private EditText campoNome;
-    private EditText campoSobrenome;
     private EditText campoTelefone;
     private EditText campoEmail;
     private AlunoDAO dao;
@@ -64,7 +63,6 @@ public class FormularioAlunoActivity extends AppCompatActivity {
 
     private void preencheCampos() {
         campoNome.setText(aluno.getNome());
-        campoSobrenome.setText(aluno.getSobrenome());
         campoTelefone.setText(aluno.getTelefone());
         campoEmail.setText(aluno.getEmail());
     }
@@ -81,7 +79,6 @@ public class FormularioAlunoActivity extends AppCompatActivity {
 
     private void inicializacaoDosCampos() {
         campoNome = findViewById(R.id.activity_formulario_aluno_nome);
-        campoSobrenome = findViewById(R.id.activity_formulario_aluno_sobrenome);
         campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
         campoEmail = findViewById(R.id.activity_formulario_aluno_email);
     }
@@ -90,10 +87,8 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         String nome = campoNome.getText().toString();
         String telefone = campoTelefone.getText().toString();
         String email = campoEmail.getText().toString();
-        String sobrenome = campoSobrenome.getText().toString();
 
         aluno.setNome(nome);
-        aluno.setSobrenome(sobrenome);
         aluno.setTelefone(telefone);
         aluno.setEmail(email);
     }
